bff70b21f5de1bfb2f760d4422ee55d5c3c94a8b
unknown <180001055@cientifica.edu.pe>
2022-04-19 12:40:43 -0500

    Daysi

---
+++ src/app/inicioPeru/inicioPeru.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InicioPeruComponent } from './inicioPeru.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [InicioPeruComponent]
+})
+export class InicioPeruModule { }
